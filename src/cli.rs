178ba47ae74e3d6ad175d5cf1ca982006b86df9b
richie <richiezhang@protonmail.com>
2021-01-06 00:14:45 -0800

    Remove CLI default arguments

--- src/cli.rs
+++ src/cli.rs
@@ -5,11 +5,10 @@ use structopt::StructOpt;
 #[derive(Debug, StructOpt)]
 #[structopt(name = "xfizzbuzz", about = "An extensible fizzbuzz implementation.")]
 pub struct Cli {
-    #[structopt(default_value = "15", help = "The maximum number to count to.")]
+    #[structopt(help = "The maximum number to count to.")]
     pub max_number: u32,
 
     #[structopt(
-        default_value = "3:fizz 5:buzz",
         help = "The words to replace the number with when it is a multiple of a number, formatted as a space-separated list of '<number>:<word>'. The order of the words specified in the arguments will be the order the words are placed if a number is qualified for more than one word."
     )]
     pub words: Vec<String>,
